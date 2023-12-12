cdf663434064b31d38c0d6f1dd9b4948882b8d43
Ebrahim <morkasebrahim3@gmail.com>
2023-12-12 19:06:47 +0530

    Project completed and is working fine and there will be no collision if two persons with same name are added and email will be sent to both the persons having same birthdate

--- birthday_wisher.py
+++ birthday_wisher.py
@@ -7,9 +7,9 @@ birthdate = pandas.read_csv("birthdates.csv")
 birthdates_dictionary = {}
 i = 0
 for row in birthdate.iterrows():
-    new_list = [birthdate.email[i], birthdate.month[i], birthdate.year[i], birthdate.day[i]]
+    new_list = [birthdate.email[i], birthdate.month[i], birthdate.year[i], birthdate.day[i], birthdate.name[i]]
     # print(new_list)
-    birthdates_dictionary[birthdate.name[i]] = new_list
+    birthdates_dictionary[i] = new_list
     i = i + 1
 
 # print(birthdates_dictionary)
@@ -18,3 +18,16 @@ for row in birthdate.iterrows():
 current_date = dt.datetime.now()
 current_day_in_english = current_date.strftime("%A")
 current_month = current_date.month
+current_day_of_month = current_date.day
+
+for value in birthdates_dictionary.values():
+    if current_day_of_month and current_month in value:
+        # print("Present")
+        email_address = "morkasebrahim3@gmail.com"
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(email_address, "alhd fcpw miau jyxb")
+        connection.sendmail(from_addr=email_address,
+                            to_addrs=f"{value[0]}",
+                            msg=f"Subject: Birthday Wish\n\n Happy birthday {value[4]}")
+        connection.close()
